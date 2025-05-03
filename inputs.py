6d8163ad8b7dd980da14dc3fd13e992553b3d076
Piotr Stanzecki <piotr.stanzecki@gmail.com>
2025-05-03 23:32:35 +0200

    setting up the simulation

---
+++ inputs.py
@@ -0,0 +1,43 @@
+import numpy as np
+def inp(Tend, T, N, shape, Amp, angle):
+    stepsT = N * (T/Tend) #converting time period to number of steps in simulation
+    deltaY = (Amp/stepsT)
+    U = []
+    match shape:
+            case "rectangle":
+                for i in range(0, N):
+                    if i <= stepsT:
+                        U.append(Amp)
+                    else:
+                        U.append(0)
+            case "triangle":
+                for i in range(0, N):
+                    if i % stepsT == 0:
+                        U.append(0)
+                    else:
+                        U.append(U[i - 1] + deltaY)
+            case "harmonic":
+                for i in range(0, N):
+                    U.append(np.sin(i * 2 * np.pi / stepsT))
+                   
+
+    return U
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,4 +1,45 @@
 
 import numpy as np
 import control as ctrl
+import matplotlib.pyplot as plt
+from inputs import *
+
+b = 0.1
+k = 2
+M = 1
+
+#State space model -- 
+'''
+States
+|x2|
+|x1|
+
+x2 - velocity
+x1 - position
+'''
+A = np.matrix([[-b/M , -k/M],[1, 0]])
+B = np.matrix([[1/M],[0]])
+C1 = np.matrix([0,1]) #position output
+C2 = np.matrix([1,0]) #velocity output
+D = np.matrix([0])
+
+X = np.matrix([[0],[0]])
+
+
+#Simulation
+
+deltaT = 0.01 #time step
+Tend = 10 #duration of a simulation
+N = int(Tend/deltaT) #number of steps
+T = 3 #time period of the input
+Ts = np.arange(0, N) * deltaT  # time in seconds
+U = inp(Tend,T,N,"harmonic", 3, 1)
+plt.plot(Ts,U)
+plt.show()
+
+
+
+
+
+
 
